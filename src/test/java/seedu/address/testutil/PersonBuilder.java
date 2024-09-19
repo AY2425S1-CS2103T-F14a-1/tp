821a77a48592788a603272690e7bd2e94b2256a6
Feng1231 <chenfeng021231@gmail.com>
2024-09-19 23:19:07 +0800

    RemarkCommand: Update generateSuccessMessage

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -16,6 +16,7 @@ public class PersonBuilder {
     public static final String DEFAULT_PHONE = "85355255";
     public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
+    public static final String DEFAULT_REMARK = "She likes aardvarks.";
 
     private Name name;
     private Phone phone;
@@ -34,7 +35,7 @@ public class PersonBuilder {
         email = new Email(DEFAULT_EMAIL);
         address = new Address(DEFAULT_ADDRESS);
         tags = new HashSet<>();
-        remark = new Remark("");
+        remark = new Remark(DEFAULT_REMARK);
     }
 
     /**
